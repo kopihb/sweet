458a34e75d8d27cc9538d89e2644a8ea886643eb
Serhiy Korynevych <>
2019-11-21 14:51:19 +0200

    include  formik

--- src/components/CreateItem/CreateItem.js
+++ src/components/CreateItem/CreateItem.js
@@ -1,5 +1,6 @@
 import  React, { Component } from 'react';
 import  './CreateItem.scss';
+import { Formik } from 'formik';
 
 import axios from 'axios';
 
@@ -25,32 +26,84 @@ class CreateItem extends Component {
 
     render (){
         return (
-            <div className="CreateItem">
-                <div className="header">Add  item </div>
-                <div className="inputItem">
-                    <label > Title</label>
-                    <input  type="text" value={this.state.title} onChange={(event) => {this.setState({title: event.target.value})}}/> <br/>
-                </div>
 
-                <div className="inputItem">
-                    <label > Content </label>
-                    <input type="text"  value={this.state.content} onChange={(event) => {this.setState({content: event.target.value})}}/><br/>
-                </div>
-                <div className="inputItem">
-                    <label > Price </label>
-                    <input type="text"  value={this.state.price} onChange={(event) => {this.setState({price: event.target.value})}}/><br/>
-                </div>
+            <Formik initialValues={{title: " ", content: "", price: ""}}>
+                {({values, handleChange, handleBlur}) => (
+                    <form>
+                        {JSON.stringify(values)}
+                        <div className="input-row">
+                            <label htmlFor="title"> Title</label>
+                            <input
+                                type="text"
+                                name="title"
+                                id="title"
+                                placeholder="Enter title"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.title}
+                            />
+                        </div>
+                        <div className="input-row">
+                            <label htmlFor="content"> content</label>
+                            <input
+                                type="text"
+                                name="content"
+                                id="content"
+                                placeholder="Enter content"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.content}
+                            />
+                        </div>
+                        <div className="input-row">
+                            <label htmlFor="content"> content</label>
+                            <input
+                                type="number"
+                                name="price"
+                                id="price"
+                                placeholder="Enter price"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.price}
+                            />
+                        </div>
+                    </form>
+                    )}
+
+            </Formik>
 
 
-                <div className="inputItemButton">
-                    <button onClick={this.createItemHandler}> Create Item </button>
-                </div>
 
 
-            </div>
 
-        )
+
+        );
     }
 }
 
 export default CreateItem;
+
+
+
+{/*<div className="CreateItem">*/}
+{/*<div className="header">Add  item </div>*/}
+{/*<div className="inputItem">*/}
+{/*<label > Title</label>*/}
+{/*<input  type="text" value={this.state.title} onChange={(event) => {this.setState({title: event.target.value})}}/> <br/>*/}
+{/*</div>*/}
+
+{/*<div className="inputItem">*/}
+{/*<label > Content </label>*/}
+{/*<input type="text"  value={this.state.content} onChange={(event) => {this.setState({content: event.target.value})}}/><br/>*/}
+{/*</div>*/}
+{/*<div className="inputItem">*/}
+{/*<label > Price </label>*/}
+{/*<input type="text"  value={this.state.price} onChange={(event) => {this.setState({price: event.target.value})}}/><br/>*/}
+{/*</div>*/}
+
+
+{/*<div className="inputItemButton">*/}
+{/*<button onClick={this.createItemHandler}> Create Item </button>*/}
+{/*</div>*/}
+
+{/*</div>*/}
